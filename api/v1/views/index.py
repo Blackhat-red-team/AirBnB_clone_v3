8c422b9f0f41fac8191464d731b1fb8e15ca56af
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-10-28 20:19:10 +0200

    fix and add sone input

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,29 +2,27 @@
 """this module handles status and stats routs"""
 from flask import jsonify
 from api.v1.views import app_views
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     """returns a JSON says the status of the API is OK"""
-    return jsonify({"status": "OK"})
+    return jsonify({"status": "OK"}), 200
 
 
 @app_views.route('/stats')
 def stats():
-    """retrieves the number of each object"""
-    from models.amenity import Amenity
-    from models.city import City
-    from models.place import Place
-    from models.review import Review
-    from models.state import State
-    from models.user import User
-    from models import storage
-
-    classes = [('amenities', Amenity), ('cities', City),
-               ('places', Place,), ('reviews', Review,),
-               ('states', State), ('users', User)]
-    stats_dict = {}
-    for key, cls_object in classes:
-        stats_dict[key] = storage.count(cls_object)
-    return jsonify(stats_dict)
+    """retrieves the number of each objects by type"""
+    classes = {"amenities": Amenity, "cities": City, "places": Place,
+               "reviews": Review, "states": State, "users": User}
+    count_dict = {}
+    for key, value in classes.items():
+        count_dict[key] = storage.count(value)
+    return jsonify(count_dict)
