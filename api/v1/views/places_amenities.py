860d05ca8cb9478c45bee6eefe17e6aed8f3cc16
Blackhat-red-team <ibrahimsaid909000@gmail.com>
2023-10-29 08:51:42 +0200

    fix betty

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -8,6 +8,7 @@ from models import storage
 from models.place import Place
 from models.amenity import Amenity
 
+
 @app_views.route('/places/<place_id>/amenities', methods=['GET'])
 def get_amenities_perds_place(place_id=None):
     """
@@ -19,15 +20,15 @@ def get_amenities_perds_place(place_id=None):
         abort(404, 'Not found')
 
     place_amenities = place_obj.amenities
-
     amenities_json = [amenity.to_dict() for amenity in place_amenities]
-
     return jsonify(amenities_json)
+
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
                  methods=['DELETE', 'POST'])
 def amenity_tis_place(place_id=None,
                       amenity_id=None):
+
     """
     Route to handle HTTP methods for a given amenity by ID
     """
